e0e251547fbb5523c86b031a0f5a1fad2a0c584b
Shayne999 <shaynejuniorn@gmail.com>
2024-04-19 15:10:41 +0200

    Almost complete

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -2,7 +2,7 @@
 
 """ Starts a Flask web application """
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
@@ -30,6 +30,20 @@ def pythoniscool(text= 'is cool'):
 def number(n):
     """ display “n is a number” only if n is an integer """
     return f'{n} is a number'
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numbertemplate(n):
+    """ display a HTML page only if n is an integer """
+    return render_template('5-number.html', n=n)
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def oddOrEven(n):
+    """ display a HTML page only if n is an integer """
+    if n % 2 == 0:
+        numbType = 'Even'
+    else:
+        numType = 'Odd'
+    return render_template('6-number_odd_or_even.html', n=n, numType=numType)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" Starts a Flask web application """
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """ display a HTML page """
+    states = sorted(list(storage.all("State").values()), key=lambda x: x['name'])
+    return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ tears down the storage """
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
